09eab179aff9de492614fb1abaaa45049b994c68
dingdingl0w0l <dingdingl0w0l@gmail.com>
2019-05-13 19:32:47 +1000

    Merge

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public Transform target;
+    public Vector2 move_direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        move_direction = (target.position - transform.position).normalized * speed;
+        rb.velocity = new Vector2(move_direction.x, move_direction.y);
+        Destroy(gameObject, 3f);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            //Debug.Log("hitted");
+            Destroy(gameObject);
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+    }
+}
