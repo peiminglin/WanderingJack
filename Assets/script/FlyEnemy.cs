fe80a55a2a25c1953ed76aad8ea19c0ead11397a
dingdingl0w0l <dingdingl0w0l@gmail.com>
2019-05-13 16:47:00 +1000

    U201: As a player, I want to have enemies so the game has more challenge. U202: As a player, I want different type of enemies, so I can play the game with different difficulties.

---
+++ Assets/script/FlyEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemy : Enemy
+{
+    public Transform target;
+    public float chase_radius;
+    public float attack_radius;
+    public Transform home_pos;
+    public GameObject bullet;
+    public float firerate;
+    public float next_fire;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        next_fire = Time.time;
+        //Physics2D.IgnoreCollision(GetComponent<Collider2D>(), bullet.GetComponent<Collider2D>());
+        //target = GameObject.FindWithTag("player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+        CheckTimeToFire();
+    }
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position)<=chase_radius && Vector3.Distance(target.position, transform.position)>attack_radius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    void CheckTimeToFire()
+    {
+        if (Time.time > next_fire && Vector3.Distance(target.position, transform.position) <= attack_radius)
+        {
+            Vector3 position = transform.position;
+            position.y += -1;
+            Instantiate(bullet, position, Quaternion.identity);
+            next_fire = Time.time + firerate;
+        }
+    }
+}
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(change);
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+
+        //Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //moveVelocity = moveInput * speed;
+    }
+
+    void MoveCharacter()
+    {
+        rb.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+
+}
