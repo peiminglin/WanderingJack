149ea5ecb63710225620c5974046cf45591eacde
dingdingl0w0l <dingdingl0w0l@gmail.com>
2019-05-28 14:09:07 +1000

    minor type error fix

--- Assets/Scripts/Barriers/Blackhole.cs
+++ Assets/Scripts/Barriers/Blackhole.cs
@@ -13,6 +13,6 @@ public class Blackhole : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 2.5, Space.Self);
+        transform.Rotate(0, 0, 2, Space.Self);
     }
 }
